c3a86a2081a54ada48d106bf969c7de4630d15f7
kirill_strelnikov <kirillstrelnikov5@gmail.com>
2025-02-16 02:19:15 +0700

    Revert "I RETURN FOR"
    This reverts commit 96bda12cedba6a9187a54390441ac10c69b581db.

--- helloworld.java
+++ helloworld.java
@@ -2,11 +2,19 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+<<<<<<< HEAD
         Scanner scanner = new Scanner(System.in);
 
         String lastname;
         lastname = scanner.nextLine();
 
         System.out.println("Your family: " + lastname); // Добавил комментарий 2
+=======
+        System.out.println("Hello and welcome!");
+
+        for (int i = 1; i <= 5; i++) {
+            System.out.println("i = " + i);
+        }
+>>>>>>> parent of 96bda12 (I RETURN FOR)
     }
 }
