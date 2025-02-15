643a3d8ed64222b476257fd57cfd5f2592b9e4e6
kirill_strelnikov <kirillstrelnikov5@gmail.com>
2025-02-16 01:27:25 +0700

    I reverse code on query a lastname

--- helloworld.java
+++ helloworld.java
@@ -1,8 +1,12 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello and welcome!");
-        for (int i = 1; i <= 5; i++) {
-            System.out.println("i = " + i);
-        }
+        Scanner scanner = new Scanner(System.in);
+
+        String lastname;
+        lastname = scanner.nextLine();
+
+        System.out.println("Your family: " + lastname);
     }
 }
